5138bbe0ccd8b3786efbe6d45a6c63fd63620b27
Jenkins <your-email@example.com>
2025-03-05 15:02:51 +0530

    New Branch Created WRB

---
+++ Scripts/LoginToMMT/Script1741166585889.groovy
@@ -0,0 +1,10 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.makemytrip.com/')
+
+WebUI.waitForElementVisible(findTestObject('Object Repository/Main Page/Page_MakeMyTrip - 1 Travel Website 50 OFF o_75db21/span_ENG_commonModal__close'), 30, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Main Page/Page_MakeMyTrip - 1 Travel Website 50 OFF o_75db21/span_ENG_commonModal__close'))
