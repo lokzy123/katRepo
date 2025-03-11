37550f54814a0a961a1c8ad3bbb09c8020ad8b84
lokzy123 <105543636+lokzy123@users.noreply.github.com>
2025-03-12 02:27:01 +0530

    Update Script1741166585889.groovy and Execute Job

--- Scripts/LoginToMMT/Script1741166585889.groovy
+++ Scripts/LoginToMMT/Script1741166585889.groovy
@@ -5,6 +5,6 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.makemytrip.com/')
 
-WebUI.waitForElementVisible(findTestObject('Object Repository/Main Page/Page_MakeMyTrip - 1 Travel Website 50 OFF o_75db21/span_ENG_commonModal__close'), 30, FailureHandling.STOP_ON_FAILURE)
+WebUI.waitForElementVisible(findTestObject('Object Repository/Main Page/Page_MakeMyTrip - 1 Travel Website 50 OFF o_75db21/span_ENG_commonModal__close'), 30, FailureHandling.OPTIONAL)
 
-WebUI.click(findTestObject('Object Repository/Main Page/Page_MakeMyTrip - 1 Travel Website 50 OFF o_75db21/span_ENG_commonModal__close'))
+// WebUI.click(findTestObject('Object Repository/Main Page/Page_MakeMyTrip - 1 Travel Website 50 OFF o_75db21/span_ENG_commonModal__close'))
