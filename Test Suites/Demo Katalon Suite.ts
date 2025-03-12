56d380dda25fcb3888abd20e8a0d4c6b5761dea0
Jenkins <your-email@example.com>
2025-03-12 13:37:52 +0530

    Added new branch and test case and collection

---
+++ Test Suites/Demo Katalon Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo Katalon Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>de4ceed0-ce57-4324-84c2-6c787ed6d951</testSuiteGuid>
+   <testCaseLink>
+      <guid>6a852ad3-ccf6-4e08-a35f-f8c65b50669e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo Katalon</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
